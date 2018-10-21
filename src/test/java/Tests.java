df61e68a5e7c25347678cb36365e7b346c6b2599
fnlfu <44083886+fnlfu@users.noreply.github.com>
2018-10-21 21:49:08 +0200

    pudliszki

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,26 +1,23 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Tests extends TestsMet{
-    @Test
-    public void testSlider() {
+public class Tests extends TestsMet {
+    @Test(dataProvider = "SliderData")
+    public void testSlider(int where) {
         driver.get("https://jqueryui.com/slider/#custom-handle");
 
         WebElement frame = driver.findElement(By.cssSelector("iframe[class='demo-frame']"));
         driver.switchTo().frame(frame);
-        WebElement Sslider = driver.findElement(By.id("custom-handle"));
+        WebElement slider = driver.findElement(By.id("custom-handle"));
 
-        int kek = 55;
-        Sslider.click();
-        if (Integer.parseInt(Sslider.getText())<kek)
-            while (Integer.parseInt(Sslider.getText())!=kek){
-                Sslider.sendKeys(Keys.ARROW_RIGHT);
-            }
-        else
-            while (Integer.parseInt(Sslider.getText())!=kek){
-                Sslider.sendKeys(Keys.ARROW_LEFT);
-            }
+        SliderMove(slider, where);
+        Assert.assertEquals(Integer.parseInt(slider.getText()), where);
+
+    }
+
+    @Test(parameters = "SliderDataAll")
+    public void testSliderAll() {
     }
 }
--- src/test/java/TestsMet.java
+++ src/test/java/TestsMet.java
@@ -1,17 +1,26 @@
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 
 public class TestsMet {
 
     WebDriver driver;
 
+    @DataProvider(name = "SliderData")
+    public static Object[][] SliderData() {
+        return new Object[][]{{80}, {50}, {55}, {55}};
+    }
+
+
     @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
-        System.setProperty("webdriver.firefox.marionette","src/test/dricers/geckodriver.exe");
+        System.setProperty("webdriver.firefox.marionette", "src/test/dricers/geckodriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
@@ -20,5 +29,16 @@ public class TestsMet {
     @AfterMethod
     public void tearDown() {
         driver.quit();
+    }
+
+    public void SliderMove(WebElement slider, int i) {
+        if (Integer.parseInt(slider.getText()) < i)
+            while (Integer.parseInt(slider.getText()) != i) {
+                slider.sendKeys(Keys.ARROW_RIGHT);
+            }
+        else
+            while (Integer.parseInt(slider.getText()) != i) {
+                slider.sendKeys(Keys.ARROW_LEFT);
+            }
     }
 }
